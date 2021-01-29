68cbd9a3fdefdf5fa7c3ee5d3383a29b9fd639b2
Matheus Mitestainer <matheusmitestainer@outlook.com>
2021-01-29 20:40:43 -0300

    Commit dia 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,48 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+// const QuizBackground = styled.div`
+//   background-image: url(${db.bg});
+//   background-size: 'cover';
+//   flex: 1;
+//   background-position: 'center'
+// `
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizzes da Galera</h1>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner />
+    </QuizBackground>
+  )
 }
